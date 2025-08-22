c614dabe2f651523988100f35172c759d12ce50e
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2025-08-22 11:49:51 +0530

    19 ms (64.57%) 26.3 MB (41.43%) .cpp

--- LeetCode/Easy/0941-valid-mountain-array/0941-valid-mountain-array.cpp
+++ LeetCode/Easy/0941-valid-mountain-array/0941-valid-mountain-array.cpp
@@ -2,22 +2,23 @@ class Solution {
 public:
     bool validMountainArray(vector<int>& arr) {
         int size = arr.size();
-        if (size < 3) return false;
-
+        if (size < 3) {
+            return false;
+        }
         int peak = 0;
 
-        while (peak + 1 < size && arr[peak] < arr[peak + 1]) {
-            peak++;
+        while (peak < size - 1 && arr[peak] < arr[peak + 1]) {
+            peak = peak + 1;
+        }
+        if (peak == 0 || peak == size - 1)
+            return false;
+
+        for (int i = peak; i < size - 1; i++) {
+            if (arr[i] <= arr[i + 1]) {
+                return false;
+            }
         }
 
-
-        if (peak == 0 || peak == size - 1) return false;
-
-
-        while (peak + 1 < size && arr[peak] > arr[peak + 1]) {
-            peak++;
-        }
-
-        return peak == size - 1;
+        return true;
     }
 };
