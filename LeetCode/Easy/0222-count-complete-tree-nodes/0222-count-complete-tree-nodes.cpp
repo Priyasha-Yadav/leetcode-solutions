a6cd6bf029ec64639e9fb7d96c6ae5c08b0ccceb
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2025-08-25 15:10:38 +0530

    0 ms (100%) 31.3 MB (43.95%) .cpp

--- LeetCode/Easy/0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ LeetCode/Easy/0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -1,17 +1,7 @@
 class Solution {
 public:
     int countNodes(TreeNode* root) {
-        if (!root) return 0;
-
-        int sum = 1; 
-
-        if (root->left) {
-            sum += countNodes(root->left); 
-        }
-        if (root->right) {
-            sum += countNodes(root->right); 
-        }
-
-        return sum;
+        if (!root) return 0;  
+        return 1 + countNodes(root->left) + countNodes(root->right);
     }
 };
