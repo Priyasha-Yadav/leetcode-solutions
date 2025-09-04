f83ee847911567bf08e02eb05a61f4c4bab1db09
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2025-09-04 07:51:21 +0530

    0 ms (100%) 31.3 MB (45.39%) .cpp

---
+++ LeetCode/Easy/0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+
+        return binarySearch(nums, target, 0, nums.size() - 1);
+    }
+
+private:
+    int binarySearch(vector<int>& nums, int target, int start, int end) {
+        int mid = start + (end - start) / 2;
+        if (target == nums[mid])
+            return mid;
+        if (end <= start)
+            return -1;
+        if (target < nums[mid]) {
+            return binarySearch(nums, target, start, mid - 1);
+
+        } else {
+            return binarySearch(nums, target, mid + 1, end);
+        }
+        return -1;
+    }
+};
