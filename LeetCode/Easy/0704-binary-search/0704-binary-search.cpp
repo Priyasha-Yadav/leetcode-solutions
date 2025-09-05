7c52949809c918891d667ed894ece12d6617993f
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2025-09-05 23:25:32 +0530

    0 ms (100%) 31.3 MB (45.35%) .cpp

--- LeetCode/Easy/0704-binary-search/0704-binary-search.cpp
+++ LeetCode/Easy/0704-binary-search/0704-binary-search.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-
         return binarySearch(nums, target, 0, nums.size() - 1);
     }
 
 private:
-    int binarySearch(vector<int>& nums, int target, int start, int end) {
+    int binarySearch(vector<int>& nums, int& target, int start, int end) {
         int mid = start + (end - start) / 2;
-        if (target == nums[mid])
+        if(end < start) return -1;
+        if (nums[mid] == target)
             return mid;
-        if (end <= start)
-            return -1;
-        if (target < nums[mid]) {
+        if (nums[mid] > target)
             return binarySearch(nums, target, start, mid - 1);
-
-        } else {
+        else {
             return binarySearch(nums, target, mid + 1, end);
         }
-        return -1;
+        return 0;
     }
 };
