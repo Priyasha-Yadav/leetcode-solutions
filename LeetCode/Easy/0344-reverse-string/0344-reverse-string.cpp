adc2787bcc888e7aa19f2dc44dc5e421af5b274a
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2025-09-11 08:30:15 +0530

    0 ms (100%) 27.3 MB (16.87%) .cpp

---
+++ LeetCode/Easy/0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s, int left, int right) {
+        if (left >= right) return;
+        std::swap(s[left], s[right]);
+        reverseString(s, left + 1, right - 1);
+    }
+    
+    void reverseString(vector<char>& s) {
+        reverseString(s, 0, s.size() - 1);
+    }
+};
