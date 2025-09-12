ed2860b38fa691c16e71ed9751fdf0876e171b80
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2025-09-13 00:47:12 +0530

    0 ms (100%) 31.3 MB (73.3%) .cpp

--- LeetCode/Easy/0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ LeetCode/Easy/0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -13,7 +13,7 @@ class Solution {
 public:
     int countNodes(TreeNode* root) {
         if(!root) return 0;
-        return countNodes(root->left) + countNodes(root->right) + 1;
+        return 1+ countNodes(root->left) + countNodes(root->right);
 
     }
 };
