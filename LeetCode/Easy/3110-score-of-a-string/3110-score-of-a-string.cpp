2418214be214dccba13e6fd5a151a598718f5e26
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2025-09-20 10:20:52 +0530

    0 ms (100%) 9 MB (27.62%) .cpp

---
+++ LeetCode/Easy/3110-score-of-a-string/3110-score-of-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int diff = 0;
+        int score = 0;
+        for(int i = 0; i< s.length() - 1; i++){
+            diff = abs(s[i+1] - s [i]);
+            score += diff;
+        }
+        return score;
+    }
+};
