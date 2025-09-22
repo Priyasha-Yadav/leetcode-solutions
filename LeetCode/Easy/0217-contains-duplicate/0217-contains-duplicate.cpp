39bd0e5800c52ffa8b1922ea9c23ab884e760bcf
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2025-09-22 13:50:47 +0530

    53 ms (44.41%) 90.9 MB (31.77%) .cpp

--- LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.cpp
+++ LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,12 +1,19 @@
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for(int i = 1; i < nums.size(); i++){
-            if(nums[i] == nums[i-1]){
-                return true;
+        unordered_set<int> seen;
+        
+        for (int num : nums) {
+            if (seen.find(num) != seen.end()) {
+                return true; 
             }
+            seen.insert(num);
         }
-        return false;
+        
+        return false; 
     }
 };
