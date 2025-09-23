d5cd4cc70fbfb9e10a222beb1ad614c76ceb587b
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2025-09-23 10:30:45 +0530

    0 ms (100%) 8.8 MB (90.55%) .cpp

---
+++ LeetCode/Easy/0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int count = 0;
+        for(int i = s.length() -1 ; i >= 0; i--){
+            if(count > 0 && s[i] == ' '){ break;}
+            if(s[i] == ' '){
+                continue;
+            }
+            else count++;
+
+
+        }
+        return count;
+    }
+};
