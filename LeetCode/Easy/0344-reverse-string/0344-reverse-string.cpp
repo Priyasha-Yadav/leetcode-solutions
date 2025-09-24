68861196fb1f096b7d17801fba0ab6b09d358b3a
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2025-09-24 23:56:58 +0530

    0 ms (100%) 27.4 MB (16.84%) .cpp

--- LeetCode/Easy/0344-reverse-string/0344-reverse-string.cpp
+++ LeetCode/Easy/0344-reverse-string/0344-reverse-string.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int st = 0;
-        int end = s.size() - 1;
-        while(end > st){
-            swap(s[end], s[st]);
-            end--;
-            st++;
-        }
-        
+       int st = 0;
+       int end = s.size() - 1;
+       while(end > st){
+        swap(s[st], s[end]);
+        end--;
+        st++;
+       }
+
     }
 };
