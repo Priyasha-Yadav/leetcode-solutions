02dbea7e402d0f1ea3e9aedb5ece0c22ca61a7d1
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2025-09-27 21:17:16 +0530

    0 ms (100%) 24.1 MB (40.6%) .cpp

--- LeetCode/Easy/0605-can-place-flowers/0605-can-place-flowers.cpp
+++ LeetCode/Easy/0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -2,29 +2,20 @@ class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
         int count = 0;
-        if(flowerbed.size() == 1) {
-            if(flowerbed[0] == 0 && (n == 1|| n == 0)) return true;
-            if(flowerbed[0] == 1 &&  n == 0) return true;
-            if(flowerbed[0] == 1 &&  n == 1) return false;
+        int size = flowerbed.size();
+        int* ptr = flowerbed.data(); 
 
-        };
+        for (int i = 0; i < size; i++) {
+            if (ptr[i] == 0) {
+                int left = (i == 0) ? 0 : ptr[i-1];
+                int right = (i == size-1) ? 0 : ptr[i+1];
 
-        if (flowerbed.size() > 1 && flowerbed[0] == 0 && flowerbed[1] == 0)
-            {count++;
-            flowerbed[0] = 1;
+                if (left == 0 && right == 0) {
+                    ptr[i] = 1; 
+                    count++;
+                }
             }
-        if (flowerbed.size() > 2 && flowerbed[flowerbed.size() - 2] == 0 &&
-             flowerbed[flowerbed.size() - 1] == 0) {
-            flowerbed[flowerbed.size() - 1] = 1;
-            count++;
-        }
-
-        for (int i = 1; i < flowerbed.size() - 1; i++) {
-            if (flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0 &&
-                flowerbed[i] == 0) {
-                flowerbed[i] = 1;
-                count++;
-            }
+           
         }
 
         return count >= n;
