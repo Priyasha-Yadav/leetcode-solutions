8cfd64494234c910f083a1b5f0127bf740698d75
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2025-09-27 13:20:49 +0530

    7 ms (7.56%) 9.2 MB (80.5%) .cpp

---
+++ LeetCode/Easy/0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        map<char, char> m;
+        map<char, char> n;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (m[s[i]]) {
+                if (m.at(s[i]) != t[i])
+                    return false;
+            }
+            m[s[i]] = t[i];
+            if (n[t[i]]) {
+                if (n.at(t[i]) != s[i])
+                    return false;
+            }
+            n[t[i]] = s[i];
+        }
+        return true;
+    }
+};
