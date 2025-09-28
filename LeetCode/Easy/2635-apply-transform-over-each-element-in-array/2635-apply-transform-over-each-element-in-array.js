989bd4e32d7651b5572c81789b5decdad8854346
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2025-09-28 15:39:36 +0530

    54 ms (6.34%) 53.8 MB (45.23%) .js

--- LeetCode/Easy/2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ LeetCode/Easy/2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -3,9 +3,9 @@
  * @param {Function} fn
  * @return {number[]}
  */
-var map = function(arr, fn) {
+var map = function (arr, fn) {
     const ans = new Array(arr.length);
-    for(let i = 0; i < arr.length; i++){
+    for (let i = 0; i < arr.length; i++) {
         ans[i] = fn(arr[i], i);
     }
     return ans;
