f7e8e30a946171d09c9c2dc2dd7657a5b1928f9d
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2025-10-04 00:54:12 +0530

    53 ms (19.12%) 54.7 MB (70.17%) .js

--- LeetCode/Easy/2626-array-reduce-transformation/2626-array-reduce-transformation.js
+++ LeetCode/Easy/2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -4,9 +4,9 @@
  * @param {number} init
  * @return {number}
  */
-var reduce = function(nums, fn, init) {
-    var res = init;
-    for(let i = 0; i < nums.length;i++){
+var reduce = function (nums, fn, init) {
+    let res = init;
+    for (let i = 0; i < nums.length; i++) {
         res = fn(res, nums[i]);
     }
     return res;
