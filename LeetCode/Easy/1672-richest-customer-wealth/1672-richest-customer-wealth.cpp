7b15693c12002cf36a6ef9721fdb38b17ada27a0
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2025-10-05 15:12:23 +0530

    0 ms (100%) 11.4 MB (56.57%) .cpp

--- LeetCode/Easy/1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ LeetCode/Easy/1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -3,12 +3,12 @@ public:
     int maximumWealth(vector<vector<int>>& accounts) {
         int maximum = 0, curr = 0;
         for (int i = 0; i < accounts.size(); i++) {
-            for (int j = 0; j < accounts[i].size(); j++) {
+            for (int j = 0; j < accounts[0].size(); j++) {
                 curr += accounts[i][j];
             }
             maximum = max(maximum, curr);
             curr = 0;
         }
-        return maximum;
+    return maximum;
     }
 };
