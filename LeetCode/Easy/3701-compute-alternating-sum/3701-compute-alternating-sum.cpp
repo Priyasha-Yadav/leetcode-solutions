b83ae9f0a15366f5d5cef1ede3afe1c719fb5bac
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2025-10-05 13:06:15 +0530

    0 ms (100%) 32.4 MB (100%) .cpp

--- LeetCode/Easy/3701-compute-alternating-sum/3701-compute-alternating-sum.cpp
+++ LeetCode/Easy/3701-compute-alternating-sum/3701-compute-alternating-sum.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int alternatingSum(vector<int>& nums) {
-        int ans = 0;
-        int odd = 0, even = 0;
+        int even = 0, odd = 0;
         for (int i = 0; i < nums.size(); i++) {
-            if(i%2 == 0) even += nums[i];
-            else odd += nums[i];
+            if (i % 2 == 0)
+                even += nums[i];
+            else
+                odd += nums[i];
         }
-        ans = even - odd;
-        return ans;
+        return even - odd;
     }
 };
