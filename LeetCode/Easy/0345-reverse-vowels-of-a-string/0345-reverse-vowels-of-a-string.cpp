022998dc92a26e51268078798920521931f0d3be
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2025-10-05 16:25:58 +0530

    3 ms (40.64%) 10.3 MB (79.05%) .cpp

--- LeetCode/Easy/0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ LeetCode/Easy/0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -3,25 +3,22 @@ public:
     string reverseVowels(string s) {
         int st = 0;
         int end = s.length() - 1;
-
         while (st < end) {
-            if (!isVowel(s[st])) {
+            if (isVowel(s[st])) {
+                if (isVowel(s[end])) {
+                    swap(s[st], s[end]);
+                    st++;
+                    end--;
+                } else
+                    end--;
+            } else
                 st++;
-            } else if (!isVowel(s[end])) {
-                end--;
-            } else {
-                swap(s[st], s[end]);
-                st++;
-                end--;
-            }
         }
-
         return s;
     }
 
 private:
-    bool isVowel(char c) {
-        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
-               c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
-    }
+    bool isVowel(char c){return c == 'a' || c == 'e' || c == 'i' || c == 'o' ||
+                                c == 'u' || c == 'A' || c == 'E' || c == 'I' ||
+                                c == 'O' || c == 'U';}
 };
