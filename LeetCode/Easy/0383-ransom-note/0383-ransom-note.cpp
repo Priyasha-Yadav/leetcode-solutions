c462f18620fa9a67e8c551a98f279162d5045f73
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2025-10-05 16:51:19 +0530

    8 ms (20.03%) 11.7 MB (89.74%) .cpp

--- LeetCode/Easy/0383-ransom-note/0383-ransom-note.cpp
+++ LeetCode/Easy/0383-ransom-note/0383-ransom-note.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        map<char, int> m;
+        unordered_map<char, int> m;
         for (char c : magazine) {
             m[c]++;
         }
