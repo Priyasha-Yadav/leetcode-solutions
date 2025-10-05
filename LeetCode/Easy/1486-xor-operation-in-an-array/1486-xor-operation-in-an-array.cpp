54ee021cec1024dcceb1c37411f7247d85e930e5
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2025-10-05 13:15:53 +0530

    2 ms (3.78%) 9.3 MB (5.1%) .cpp

--- LeetCode/Easy/1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
+++ LeetCode/Easy/1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int xorOperation(int n, int start) {
         vector<int> nums;
-        int ans = 0;
         nums.resize(n);
+        int ans = 0;
         for (int i = 0; i < n; i++) {
             nums[i] = start + 2 * i;
             ans ^= nums[i];
