5e9a3556f20df460f8ae62c39cbdcdf8add3ac56
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2025-10-11 04:00:09 +0530

    46 ms (91.86%) 55.6 MB (48.67%) .js

--- LeetCode/Easy/2629-function-composition/2629-function-composition.js
+++ LeetCode/Easy/2629-function-composition/2629-function-composition.js
@@ -2,10 +2,10 @@
  * @param {Function[]} functions
  * @return {Function}
  */
-var compose = function (functions) {
-
-    return function (x) {
-        for (let i = functions.length - 1; i >= 0; i--) {
+var compose = function(functions) {
+    
+    return function(x) {
+        for(let i = functions.length - 1; i >= 0; i--){
             x = functions[i](x);
         }
         return x;
