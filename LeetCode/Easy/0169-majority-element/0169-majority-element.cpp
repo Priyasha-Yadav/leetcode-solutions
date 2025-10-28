b0669f2f31161520700b39d109b2c98c3097ef56
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2025-10-28 21:33:32 +0530

    7 ms (11.15%) 28.2 MB (28.74%) .cpp

--- LeetCode/Easy/0169-majority-element/0169-majority-element.cpp
+++ LeetCode/Easy/0169-majority-element/0169-majority-element.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        map<int, int> m;
-        int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            m[nums[i]]++;
-        if(m.at(nums[i]) > n/2)
-            return nums[i];
-        }
-        return 0;
+        sort(nums.begin(), nums.end());
+        return nums[nums.size()/2];
     }
 };
