9e6504b59c16bfcf2b3f54a3bbc87a7696705e3a
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2025-11-14 14:34:42 +0530

    0 ms (100%) 8.9 MB (66.3%) .cpp

--- LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.cpp
+++ LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -3,16 +3,17 @@ public:
     bool isValid(string s) {
         stack<char> check;
         for (char c : s) {
-            if (c == '(' || c == '{' || c == '[') {
+            if (c == '{' || c == '[' || c == '(')
                 check.push(c);
-            } else {
-                 if (check.empty()) return false;
-                if (c == ')' && check.top() == '(' ||
-                    c == ']' && check.top() == '[' ||
-                    c == '}' && check.top() == '{')
-                    check.pop();
-                else
+            else {
+                if (check.empty())
                     return false;
+                if (check.top() == '(' && c == ')' ||
+                    check.top() == '[' && c == ']' ||
+                    check.top() == '{' && c == '}') {
+                    check.pop();
+                }
+                else return false;
             }
         }
         return check.empty();
