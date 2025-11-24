85160134f44929660c7281724745bd1396cf33a1
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2025-11-24 12:10:02 +0530

    0 ms (100%) 14.8 MB (52.22%) .cpp

--- LeetCode/Easy/0001-two-sum/0001-two-sum.cpp
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.cpp
@@ -1,32 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int st = 0;
-        int end = nums.size() - 1;
-        vector<int> copy = nums;
-        vector<int> result;
-        sort(nums.begin(), nums.end()); 
+        unordered_map<int, int> mp; 
         
-        while (st < end) {
-            int sum = nums[st] + nums[end];
-            if (sum == target) {
-                result.push_back(nums[st]);
-                result.push_back(nums[end]);
-                break;
-            } else if (sum > target)
-                end--;
-            else
-                st++;
-        }
-
-        vector<int> indices;
-        for (int i = 0; i < copy.size(); i++) {
-            if (copy[i] == result[0] || copy[i] == result[1]) {
-                indices.push_back(i);
-                if (indices.size() == 2) break; 
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            
+            if (mp.count(complement)) {
+                return {mp[complement], i};
             }
+
+            mp[nums[i]] = i;
         }
 
-        return indices;
+        return {};
     }
 };
