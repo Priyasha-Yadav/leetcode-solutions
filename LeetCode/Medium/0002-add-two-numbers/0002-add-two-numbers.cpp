218ea77319acbd6ae83e8fbdb3dcc5c427aa031d
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2025-11-24 15:16:11 +0530

    0 ms (100%) 77.2 MB (44.68%) .cpp

--- LeetCode/Medium/0002-add-two-numbers/0002-add-two-numbers.cpp
+++ LeetCode/Medium/0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -13,10 +13,9 @@ public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         ListNode* result = new ListNode(0);
         ListNode* ptr = result;
-        int carry = 0;
-        int sum = 0;
+        int carry = 0, sum = 0;
         while (l1 || l2 || carry) {
-            int sum = sum + carry;
+            int sum = carry;
             if (l1) {
                 sum += l1->val;
                 l1 = l1->next;
@@ -25,13 +24,10 @@ public:
                 sum += l2->val;
                 l2 = l2->next;
             }
-
             carry = sum / 10;
-            sum = sum % 10;
-            ListNode* temp = new ListNode(sum);
-            sum = 0;
-            ptr->next = temp;
+            ptr->next = new ListNode(sum % 10);
             ptr = ptr->next;
+           
         }
         return result->next;
     }
