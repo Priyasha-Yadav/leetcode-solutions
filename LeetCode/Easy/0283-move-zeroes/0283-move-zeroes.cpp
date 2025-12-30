651844af46698d01f5ab46560b4e771df75514d6
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2025-12-30 11:02:02 +0530

    0 ms (100%) 23.9 MB (19.41%) .cpp

--- LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.cpp
+++ LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n = nums.size(); 
-        int nonZeroIndex = 0; 
-        
-        for (int i = 0; i < n; i++) {
+        int zero = 0;
+        for (int i = 0; i < nums.size(); i++) {
             if (nums[i] != 0) {
-                nums[nonZeroIndex] = nums[i];
-                nonZeroIndex++;
+                swap(nums[i], nums[zero]);
+                zero++;
             }
-        }
-
-        for (int i = nonZeroIndex; i < n; i++) {
-            nums[i] = 0;
         }
     }
 };
