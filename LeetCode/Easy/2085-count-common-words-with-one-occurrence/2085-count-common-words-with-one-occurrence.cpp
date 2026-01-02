8297d6333e65e2f7408f5c818fe654d7bd98a616
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2026-01-02 16:23:08 +0530

    29 ms (10.23%) 22.8 MB (54.4%) .cpp

--- LeetCode/Easy/2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.cpp
+++ LeetCode/Easy/2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.cpp
@@ -2,16 +2,18 @@ class Solution {
 public:
     int countWords(vector<string>& words1, vector<string>& words2) {
         map<string, int> m;
+        map<string, int> n;
         int count = 0;
         for (int i = 0; i < words1.size(); i++) {
             m[words1[i]]++;
         }
 
         for (int i = 0; i < words2.size(); i++) {
-            m[words2[i]]--;
+            n[words2[i]]++;
         }
+
         for (int i = 0; i < words2.size(); i++) {
-            if(m[words2[i]] == 0 ) count++;
+            if(n[words2[i]] == 1 && m[words2[i]] == n[words2[i]]) count++;
         }
         return count;
     }
