c833a50d43b3d3835e929c22d9383fbee0a4e8a3
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2026-01-05 11:45:07 +0530

    3 ms (95.42%) 12.5 MB (18.95%) .cpp

--- LeetCode/Medium/0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ LeetCode/Medium/0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -9,7 +9,10 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
+        ListNode* ptr = node->next;
         node->val = node->next->val;
         node->next = node->next->next;
+        delete ptr;
+
     }
 };
