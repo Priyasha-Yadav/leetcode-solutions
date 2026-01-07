56f56d667b681f260b651cbe43e2da1cf82929a4
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2026-01-07 09:42:14 +0530

    0 ms (100%) 13.4 MB (6.81%) .cpp

---
+++ LeetCode/Medium/0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+        for (int i = 0; i < matrix.size(); i++) {
+            if (binaryFind(matrix[i], target))
+                return true;
+        }
+        return false;
+    }
+
+private:
+    bool binaryFind(vector<int> nums, int target) {
+        int low = 0, high = nums.size() - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target)
+                return true;
+            else if (nums[mid] > target)
+                high = mid - 1;
+            else if (nums[mid] < target)
+                low = mid + 1;
+        }
+        return false;
+    }
+};
