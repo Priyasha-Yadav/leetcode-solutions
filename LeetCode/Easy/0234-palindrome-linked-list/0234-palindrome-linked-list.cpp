7daa7a73702a0720bca0388e195f200415cdaf8d
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2026-01-10 10:41:57 +0530

    1 ms (67.9%) 121.8 MB (46.4%) .cpp

--- LeetCode/Easy/0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ LeetCode/Easy/0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,43 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        if (!head || !head->next) return true;
-
+        if(!head || !head->next) return true;
         ListNode* slow = head;
+        ListNode* prev = head;
         ListNode* fast = head;
-        ListNode* prev = nullptr;
-
-
         while (fast && fast->next) {
             fast = fast->next->next;
             prev = slow;
             slow = slow->next;
         }
-
+        if (fast) slow = slow->next;
 
         prev->next = reverse(slow);
-
-        ListNode* left = head;
-        ListNode* right = prev->next;
-
-
-        while (left != prev->next) {
-            if (left->val != right->val) return false;
-            left = left->next;
-            right = right->next;
+        ListNode* ptr = prev->next;
+        while(head != prev->next){
+            if(head->val != ptr->val) return false;
+            ptr = ptr->next;
+            head = head->next;
         }
-
         return true;
     }
 
 private:
     ListNode* reverse(ListNode* head) {
+        ListNode* next = head;
         ListNode* prev = nullptr;
-        while (head) {
-            ListNode* next = head->next;
-            head->next = prev;
-            prev = head;
-            head = next;
+        ListNode* curr = next;
+        while (next) {
+            next = next->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
         }
         return prev;
     }
