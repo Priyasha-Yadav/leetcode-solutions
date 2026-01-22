2fba27226de4204601917e32091fd4c8c9b820ea
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2026-01-22 11:16:27 +0530

    0 ms (100%) 9.2 MB (37.77%) .cpp

--- LeetCode/Easy/0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ LeetCode/Easy/0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        if (needle.empty())
-            return 0;
+        if(needle.empty()) return 0;
+        int h = 0 , n = 0;
+        int start = 0;
 
-        if (haystack.size() < needle.size())
-            return -1;
-
-        int h = 0, n = 0, idx = -1;
-
-        while (h < haystack.size()) {
-            if (haystack[h] == needle[n]) {
-                if (idx == -1)
-                    idx = h;
-                h++;
+        while(h < haystack.size()){
+            if(haystack[h] == needle[n]){
                 n++;
-                if (n == needle.size())
-                    return idx;
-            } else {
-                if (idx != -1)
-                    h = idx + 1;
-                else
-                    h++;
-
-                idx = -1;
+                h++;
+                if(n == needle.size()) return start;
+            }else{
+                start++;
+                h = start;
                 n = 0;
-            }
+            }                          
         }
         return -1;
     }
