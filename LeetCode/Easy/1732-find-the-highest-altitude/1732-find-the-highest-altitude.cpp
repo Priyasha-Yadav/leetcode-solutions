daba0ce9401475c0d17694eae30eefb68e8a6252
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2026-01-30 14:02:30 +0530

    0 ms (100%) 10.8 MB (44.7%) .cpp

--- LeetCode/Easy/1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
+++ LeetCode/Easy/1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
-        int p = 0;
-        for (int i = 0; i < gain.size(); i++) {
-            int curr = gain[i];
-            gain[i] = p;
-            p += curr;
+        int altitude = 0;
+        int highest = 0;
+
+        for (int g : gain) {
+            altitude += g;
+            highest = max(highest, altitude);
         }
-        int max = INT_MIN;
-        for (int i : gain) {
-            if (i > max)
-                max = i;
-        }
-        return max;
+
+        return highest;
     }
 };
