a6d2f3ae4f38dd79e252a95b709fbdc87888bc35
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2026-01-30 10:04:30 +0530

    0 ms (100%) 19.6 MB (31.83%) .cpp

---
+++ LeetCode/Medium/0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l = 0, h = numbers.size() - 1;
+        while (l <= h) {
+            int s = numbers[l] + numbers[h];
+            if (s == target)
+                return {l + 1, h + 1};
+            else if (s < target)
+                l++;
+            else if (s > target)
+                h--;
+        }
+        return {};
+    }
+};
