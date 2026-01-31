ecdcd54b9aeb055ec0feb0e1bf9c072a889f341e
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2026-02-01 00:14:01 +0530

    16 ms (59.79%) 26 MB (48.29%) .cpp

---
+++ LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> m;
+        vector<vector<string>> res;
+        for (string s : strs) {
+            string str = s;
+            sort(str.begin(), str.end());
+            m[str].push_back(s);
+        }
+        for (auto it : m) {
+            res.push_back(it.second);
+        }
+        return res;
+    }
+};
