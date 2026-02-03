189543bd3f02a8b581fb4485df30ab4b5d338123
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2026-02-03 10:25:49 +0530

    0 ms (100%) 27.8 MB (10.15%) .cpp

--- LeetCode/Easy/3637-trionic-array-i/3637-trionic-array-i.cpp
+++ LeetCode/Easy/3637-trionic-array-i/3637-trionic-array-i.cpp
@@ -2,35 +2,24 @@ class Solution {
 public:
     bool isTrionic(vector<int>& nums) {
         int n = nums.size();
-        int p = 0, q = n - 1;
-        while (1) {
-            if (p + 1 <= n - 1 && nums[p] < nums[p + 1])
-                p++;
-            else if (p + 1 <= n - 1 && nums[p] == nums[p + 1])
-                return false;
-            else
-                break;
-        }
-
-        while (1) {
-            if (q - 1 >= 0 && nums[q] > nums[q - 1])
-                q--;
-            else if (p + 1 <= n - 1 && nums[q] == nums[q - 1])
-                return false;
-            else
-                break;
-        }
-        if (p == n - 1 || q == 0 || p == 0 || q == n - 1)
+        if (n < 4)
             return false;
-        while (1) {
-            if (nums[p] > nums[q]) {
-                p++;
-                q--;
-            } else if (q == p || p > q)
-                return true;
-            else
-                break;
-        }
-        return false;
+        int i = 0;
+        while (i + 1 < n && nums[i] < nums[i + 1])
+            i++;
+        if (i == 0)
+            return false;
+
+        int mid = i;
+        while (i + 1 < n && nums[i] > nums[i + 1])
+            i++;
+        if (i == mid)
+            return false;
+
+        int start = i;
+        while (i + 1 < n && nums[i] < nums[i + 1])
+            i++;
+if( i == start) return false;
+        return i == n - 1;
     }
 };
