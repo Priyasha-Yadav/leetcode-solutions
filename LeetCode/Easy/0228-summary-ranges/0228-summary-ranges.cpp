0a3ef3d84d6a9127b2f60f1970b3c931ad5e94ae
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2026-02-04 23:24:20 +0530

    N/A (0%) N/A (0%) .cpp

---
+++ LeetCode/Easy/0228-summary-ranges/0228-summary-ranges.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0)
+            return {};
+        vector<string> res;
+        int i = 0;
+        for (int i = 0; i < n; i++) {
+            int start = nums[i];
+            while (i + 1 < n && nums[i + 1] - nums[i] == 1)
+                i++;
+            int end = nums[i];
+            if (start == end)
+                res.push_back(to_string(end));
+            else
+                res.push_back(to_string(start) + "->" + to_string(end));
+        }
+        return res;
+    }
+};
