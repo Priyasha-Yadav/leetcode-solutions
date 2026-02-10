84598d72dd298ccbf68363b67dc5e0421fbbe472
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2026-02-10 10:53:31 +0530

    0 ms (100%) 14.9 MB (18.82%) .cpp

--- LeetCode/Easy/0001-two-sum/0001-two-sum.cpp
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        map<int, int> m;
+        unordered_map<int, int> m;
         for (int i = 0; i < nums.size(); i++) {
             int diff = target - nums[i];
-            if (m.find(diff) != m.end())
-                return {i, m[diff]};
-            else
+            if (m.find(diff) != m.end()) {
+                return { m[diff], i};
+            } else
                 m[nums[i]] = i;
         }
         return {};
