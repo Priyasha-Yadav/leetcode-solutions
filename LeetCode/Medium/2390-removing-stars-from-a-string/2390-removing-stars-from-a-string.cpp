ab3410bb362c9667fb7d9ece334e1cf6e70e56d8
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2026-02-15 15:37:08 +0530

    18 ms (57.63%) 29.7 MB (34.08%) .cpp

---
+++ LeetCode/Medium/2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        stack<char> st;
+        for (char c : s) {
+            if (c == '*') {
+                if (!st.empty())
+                    st.pop();
+                else continue;
+            }
+            else st.push(c);
+        }
+        string res = "";
+        while(!st.empty()) {
+            res+= st.top();
+            st.pop();
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
