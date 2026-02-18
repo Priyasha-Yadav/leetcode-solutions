5dc354b6e5c3d90a0ce0e4b7519922271fcd8752
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2026-02-18 12:58:41 +0530

    4 ms (79.21%) 20.2 MB (10.49%) .cpp

---
+++ LeetCode/Easy/0929-unique-email-addresses/0929-unique-email-addresses.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int numUniqueEmails(vector<string>& emails) {
+        unordered_map<string, int> m;
+        for (string e : emails) {
+            string email = localName(e) + domain(e);
+            m[email]++;
+        }
+        return m.size();
+    }
+
+private:
+    string localName(string s) {
+        string res = "";
+        for (char c : s) {
+            if (c == '.')
+                continue;
+            else if (c == '+' || c == '@')
+                break;
+            else
+                res += c;
+        }
+        return res;
+    }
+
+private:
+    string domain(string s) {
+        int idx = 0;
+        while (1)
+            if (s[idx++] == '@')
+                break;
+        return s.substr(idx + 1);
+    }
+};
