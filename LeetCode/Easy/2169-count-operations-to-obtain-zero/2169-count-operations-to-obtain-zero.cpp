1dcf1466284214f5a782aed7cf9123afe41ec287
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2026-02-18 09:40:28 +0530

    N/A (0%) N/A (0%) .cpp

---
+++ LeetCode/Easy/2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        if (num1 == 0 || num2 == 0)
+            return 0;
+        else if (num1 == num2)
+            return 1;
+        int c = 0;
+        while (num1 && num2) {
+            if (num1 > num2)
+                num1 -= num2;
+            else
+                num2 -= num1;
+        }
+        return c;
+    }
+};
