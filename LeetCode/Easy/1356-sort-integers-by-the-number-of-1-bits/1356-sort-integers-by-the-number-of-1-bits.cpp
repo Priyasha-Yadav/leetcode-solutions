455313e0ba023129a9b1f51b529bc1bf8c23201b
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2026-02-25 10:50:28 +0530

    7 ms (19.88%) 14.8 MB (26.68%) .cpp

--- LeetCode/Easy/1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ LeetCode/Easy/1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,32 +1,21 @@
 class Solution {
 public:
     vector<int> sortByBits(vector<int>& arr) {
-        vector<vector<int>> buckets (32);
+        vector<vector<int>> buckets(33);
         vector<int> res;
-        for(int i : arr){
-            int idx = countBits(i);
-            buckets[idx].push_back(i);
+
+        for (int num : arr) {
+            int bits = __builtin_popcount(num);
+            buckets[bits].push_back(num);
         }
-        for(auto& bucket : buckets){
-            if(bucket.size() == 1) res.push_back(bucket[0]);
-            else if(bucket.size() > 1) {
-                sort(bucket.begin(), bucket.end());
-                for(int i : bucket){
-                    res.push_back(i);
-                }
+
+        for (auto &bucket : buckets) {
+            sort(bucket.begin(), bucket.end());
+            for (int num : bucket) {
+                res.push_back(num);
             }
         }
 
         return res;
-    }
-
-private:
-    int countBits(int n) {
-        int count = 0;
-        while (n) {
-            count += n % 2;
-            n /= 2;
-        }
-        return count;
     }
 };
