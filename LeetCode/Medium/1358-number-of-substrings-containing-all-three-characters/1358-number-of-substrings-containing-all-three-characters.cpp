3280e49ad3dc1f651c0927ec8c7804800f65c361
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2026-02-26 13:06:51 +0530

    13 ms (34.38%) 10.8 MB (94.79%) .cpp

--- LeetCode/Medium/1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ LeetCode/Medium/1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        unordered_map<char, int> m;
+        vector<int> last(3, -1);
         int count = 0;
+
         for (int i = 0; i < s.length(); i++) {
-            m[s[i]] = i;
-            if (m.size() == 3)
-                count = count + (1 + min(m['a'], min(m['b'], m['c'])));
+            last[s[i] - 'a'] = i;
+
+            if (last[0] != -1 && last[1] != -1 && last[2] != -1) {
+                count += 1 + min(last[0], min(last[1], last[2]));
+            }
         }
+
         return count;
     }
 };
