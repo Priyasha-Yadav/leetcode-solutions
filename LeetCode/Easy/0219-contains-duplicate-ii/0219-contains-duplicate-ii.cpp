260f3d91779c5755955731c40f3c8c6633fe67ee
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2026-03-05 14:18:24 +0530

    N/A (0%) N/A (0%) .cpp

--- LeetCode/Easy/0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ LeetCode/Easy/0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -2,16 +2,18 @@ class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         unordered_set<int> window;
+        int left = 0;
+        for (int i = 0; i < nums.size(); i++) {
 
-        for(int i = 0; i < nums.size(); i++) {
-
-            if(window.count(nums[i]))
+            if (window.count(nums[i]))
                 return true;
 
             window.insert(nums[i]);
 
-            if(window.size() > k)
-                window.erase(nums[i - k]);
+            if (window.size() > k) {
+                window.erase(nums[left]);
+                left++;
+            }
         }
 
         return false;
