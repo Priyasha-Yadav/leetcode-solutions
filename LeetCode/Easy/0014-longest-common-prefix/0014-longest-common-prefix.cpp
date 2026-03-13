cce4de02a6c76ffb65eaaaf27c37e3781f9f30f5
Priyasha Yadav <priyasha.yadav.cg@gmail.com>
2026-03-14 01:00:50 +0530

    N/A (0%) N/A (0%) .cpp

--- LeetCode/Easy/0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ LeetCode/Easy/0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty()) return "";
         string prefix = strs[0];
-        for (int i = 1; i < strs.size(); i++) {
-            while (strs[i].find(prefix) != 0) {
+        for (string s : strs) {
+            while (s.find(prefix) != 0) {
                 prefix.pop_back();
                 if (prefix == "")
                     break;
